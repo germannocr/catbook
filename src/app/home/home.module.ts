89f5131c5b928aaa0719d8f5b2833b1f2b87b23c
germanno <germannoccd@hotmail.com>
2022-01-03 11:37:36 -0300

    feat: Adiciona modulos de autenticaçao, listagem de animais e formulario de login

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,17 +1,24 @@
+import { MensagemModule } from './../componentes/mensagem/mensagem.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
-  ]
+    HomeRoutingModule,
+    FormsModule,
+    MensagemModule
+  ],
+  exports: [HomeComponent],
 })
 export class HomeModule { }
